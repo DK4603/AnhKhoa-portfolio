d0d6f09c259b007dad2b352ac4745dc65602dbe5
Anh Khoa <dnk4603@gmail.com>
2026-03-12 13:18:25 +0700

    init: create portfolio project structure

---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once __DIR__ . '/../app/controllers/HomeController.php';
+
+$request = $_SERVER['REQUEST_URI'];
+
+switch ($request) {
+
+    case '/portfolio':
+    case '/portfolio/':
+
+        $controller = new HomeController();
+        $controller->index();
+        break;
+
+    default:
+        echo "404 Not Found";
+        break;
+}
