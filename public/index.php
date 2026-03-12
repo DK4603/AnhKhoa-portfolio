967a7927cefd38acecb8cc9e233a9a644c81fb48
Anh Khoa <dnk4603@gmail.com>
2026-03-12 15:46:44 +0700

    fix index

--- public/index.php
+++ public/index.php
@@ -1,7 +1,5 @@
 <?php
-//echo $_SERVER['DOCUMENT_ROOT'];
-echo __DIR__;
-exit;
-//require_once __DIR__ . '/../app/core/autoload.php';
 
-//require_once __DIR__ . '/../routes/web.php';
+require_once __DIR__ . '/../app/core/autoload.php';
+
+require_once __DIR__ . '/../routes/web.php';
